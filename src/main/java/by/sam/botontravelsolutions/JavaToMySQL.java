2e616252343d34e9ff624102bff37d7f49156801
Spawn <alkspawn@gmail.com>
2019-10-23 06:51:15 +0300

    start

--- src/main/java/by/sam/botontravelsolutions/JavaToMySQL.java
+++ src/main/java/by/sam/botontravelsolutions/JavaToMySQL.java
@@ -4,11 +4,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Arrays;
 
 /**
  * Simple Java program to connect to MySQL database running on localhost and
@@ -16,22 +13,36 @@ import java.sql.Statement;
  * @author Javin Paul
  */
 public class JavaToMySQL {
+    private static ResultSet rs;
+
 
 
     public static void main(String[] args) {
         String url = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7309426";
         String username = "sql7309426";
         String password = "Pf3yT4C76b";
+        String query = "SELECT * FROM City";
 
         System.out.println("Connecting database...");
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Database connected!");
+            Statement stmt = connection.createStatement();
+            stmt.execute(query);
+
+
+            // executing SELECT query
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int year = rs.getInt(2);
+                String name = rs.getString(1);
+                String descr = rs.getString(3);
+                System.out.printf("year: %d, name: %s, descr: %s %n", year, name, descr);
+            }
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            e.printStackTrace();
         }
+
     }
-
-
-
-}
+    }
